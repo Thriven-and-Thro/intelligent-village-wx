fd7522b05475783ad7aa422d6784d37774d1d3a6
Neo Chen <475392583@qq.com>
2023-03-26 15:56:21 +0800

    complete view of article

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,5 +1,7 @@
 // pages/detail/detail.js
-const { getStorage } = require('../../utils/cache')
+const { getStorage } = require('../../utils/cache');
+const { formateDate } = require('../../utils/formateDate');
+const { request } = require('../../utils/request')
 
 Page({
 
@@ -7,16 +9,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    content: 'hfjksdfhjksdfhksd',
-    date: '2023/03/21',
-    user: {
-      name: 'a',
-      avatar: '/static/icon/default_avatar.jpg'
+    detail: {
+      fee_id: 0,
+      content: "",
+      user_id: 0,
+      aid: 0,
+      updateTime: "",
+      state: 0,
+      area_reply: "",
+      user_reply: ""
     },
-    state: 1,
-    area_reply: 'ahdaskjdhasjkdh',
-    user_reply: "",
-    textByState: '待处理',
+    date: '',
+    user: {},
+    textByState: '',
     display: false
   },
 
@@ -24,26 +29,37 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
     // todo 请求数据
+    request("/feedback/" + options.fee_id, "GET").then(res => {
+      const detail = res
 
-    // 处理标签
-    if (this.data.state === 1) {
-      this.setData({
-        textByState: '已处理'
-      })
-    } else if (this.data.state === 2) {
-      this.setData({
-        textByState: '已拒绝'
-      })
-    }
+      // 处理标签
+      let textByState = "待处理"
+      if (detail.state === 1) {
+        textByState = '已处理'
+      } else if (detail.state === 2) {
+        textByState = '已拒绝'
+      }
 
-    // 处理发表输入框
-    if (this.data.state !== 0 && this.data.user_reply.length === 0 && getStorage('user').name === this.data.user.name) {
       this.setData({
-        display: true
+        detail,
+        textByState,
+        date: formateDate(detail.updateTime)
       })
-    }
+
+      request("/user/" + detail.user_id, "GET").then((res) => {
+        // 处理发表输入框
+        let display = false
+        if (detail.state !== 0 && detail.user_reply.length === 0 && getStorage('user').name === res?.name) {
+          display = true
+        }
+
+        this.setData({
+          user: res,
+          display
+        })
+      })
+    })
   },
 
   /**
